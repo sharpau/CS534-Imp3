0a98d92fcd9626bca01477b76277e7e76b463035
Austin Sharp <austinsharp111@gmail.com>
2013-05-21 21:05:33 -0700

    test data, comments, pseudocode, etc

--- CS534-Imp3/CS534-Imp3.cpp
+++ CS534-Imp3/CS534-Imp3.cpp
@@ -6,8 +6,35 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <assert.h>
 
 using namespace std;
+
+/* input: 
+		bagging:
+			examples only
+			[use equal weights]
+		boosting:
+			examples
+			weights on examples
+ output: hypothesis [feature index and pos / neg]
+ */
+pair<int, bool> decisionStump(vector<pair<int, vector<int>>> examples, vector<double> weights) {
+	assert(examples.size() == weights.size());
+
+}
+
+// inputs: test, training, ensemble size??
+// output: error
+void boost(void) {
+
+}
+
+// inputs: test, training, ensemble size??
+// output: error (weighted or simple count??)
+void bag(void) {
+
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -34,26 +61,27 @@ int _tmain(int argc, _TCHAR* argv[])
 		trainingData.push_back(make_pair(y, x));
 	}
 
+	vector<pair<int, vector<int>>> testData;
+	// read in data
+	ifstream testFile("SPECT-test.csv");
+	// advance to data
+	testFile >> line;
+	while(line != "@data") {
+		testFile >> line;
+	}
+
+	while(testFile.good()) {
+		// get class
+		int y;
+		testFile >> y;
+		// get features
+		vector<int> x(22, 0);
+		for(int i = 0; i < 22; i++) {
+			testFile.get();
+			testFile >> x[i];
+		}
+		testData.push_back(make_pair(y, x));
+	}
+
 	return 0;
-}
-
-/* input: 
-		bagging:
-			examples only
-			[use equal weights]
-		boosting:
-			examples
-			weights on examples
- output: hypothesis
- */
-void decisionStump(void) {
-
-}
-
-void boost(void) {
-
-}
-
-void bag(void) {
-
 }
